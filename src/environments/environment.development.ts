2f0e78df1cd5595a65543a83b0b39026285574de
Jaded Enrique Ruiz Pech <jaded.ruiz@yucatan.gob.mx>
2024-08-20 17:24:35 -0600

    Reset repo

--- src/environments/environment.development.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    // API_URL: 'https://api-reportes.grupo-scr.com.mx/api/'
-    API_URL: 'http://localhost:8000/api/'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    API_URL: 'https://api-reportes.grupo-scr.com.mx/api/'
-};
-
