9e17faa80b9f268508f16903acf03251fbfd8cf0
Jaded Enrique Ruiz Pech <jaded.ruiz@yucatan.gob.mx>
2024-08-27 16:53:33 -0600

    Actualizacion

--- src/app/admin/proyectos/proyectos.component.ts
+++ src/app/admin/proyectos/proyectos.component.ts
@@ -1,9 +1,12 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormGroup, FormsModule, NgControl, ReactiveFormsModule } from '@angular/forms';
+import { Form, FormGroup, FormsModule, NgControl, ReactiveFormsModule } from '@angular/forms';
 import { NgbCollapse, NgbCollapseModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { RxFormBuilder, RxReactiveFormsModule } from '@rxweb/reactive-form-validators';
 import { Etapa } from '../../core/models/etapa.model';
 import { NgIf } from '@angular/common';
+import { Lote } from '../../core/models/lote.model';
+import { PlazoForm } from '../../core/models/plazo.form.model';
+import { LoteForm } from '../../core/models/lote.form.model';
 
 @Component({
   selector: 'app-proyectos',
@@ -21,19 +24,41 @@ import { NgIf } from '@angular/common';
 })
 export class ProyectosComponent {
   isCollapsed = true;
+  isCollapsedLote = false;
   submitted = false;
+  submittedLote= false;
+  submittedPlazo= false;
   @ViewChild('collapse') collapse ?: NgbCollapse ;
   formEtapa !: FormGroup;
-  arrayEtapas : Etapa[] = [];
+  formLote !: FormGroup;
+  arrayEtapas: Etapa[] = [
+    {
+      sEtapa: "Etapa No.6",
+      iEtapa: 6,
+      sSvg: "/assets/Imagenes/img-default.png",
+      iTotalLotes: 0,
+      bActivo: true,
+      bActive: true
+    }
+  ];
+  arrayLotes: Lote[] = [];
+  etapaSeleccionada = {
+    seleccionado: false,
+    etapa: new Etapa,
+    lotes: this.arrayLotes
+  }
+  
+  plazo = new PlazoForm();
+  lote = new LoteForm();
 
   constructor(private _formBuilder: RxFormBuilder) { }
 
   ngOnInit() {
     this.formEtapa = this._formBuilder.formGroup(Etapa);
-    console.log(this.formEtapa.value);
+    this.formLote = this._formBuilder.formGroup(LoteForm);
   }
 
-  guardarEtapa() {
+  nuevaEtapa() {
     this.submitted = true;
     if(!this.formEtapa.valid) {
       return;
@@ -41,14 +66,23 @@ export class ProyectosComponent {
     //Aqui se guarda en la BD
     //Se agrega al Arreglo
     this.formEtapa.controls["bActive"].setValue(true);
-    console.log(this.formEtapa.value);
+    this.etapaSeleccionada.seleccionado = true;
+    this.etapaSeleccionada.etapa = this.formEtapa.value;
     this.arrayEtapas.push(this.formEtapa.value);
     this.formEtapa.reset();
     this.submitted=false;
+    this.collapse?.toggle(false);
   }
 
   editar(data : any) {
-    console.log(data);
     this.collapse?.toggle(true);
+  }
+
+  nuevoLote() {
+    this.submittedLote= true;
+    if(!this.formLote.valid){
+      return;
+    }
+    console.log(this.formLote.value);
   }
 }
