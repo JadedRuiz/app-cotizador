5ce054c98ebe48db3861c623f4a8c9936fbd0eda
Jaded Enrique Ruiz Pech <jaded.ruiz@yucatan.gob.mx>
2024-09-17 11:30:22 -0600

    Cambios Maquina 2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    // API_URL: 'https://api-cotizador.sanjeronimo.com.mx/api/',
+    //API_URL: 'https://api-cotizador.sanjeronimo.com.mx/api/',
     API_URL: 'http://localhost:8000/api/',
     iIdProyecto: 1 
 };
