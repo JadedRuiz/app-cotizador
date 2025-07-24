dfbe228fd9f70f159b96992e6866e6c2cd1fc1f4
Jaded Ruiz <razonable3500@gmail.com>
2025-07-23 20:26:04 -0600

    Mis cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     // API_URL: 'https://api-cotizador.sanjeronimo.com.mx/api/',
     // API_URL: 'http://localhost:8000/api/',
     API_URL: 'https://cotizador.zibaapartments.com/back/public/api/',
-    iIdProyecto: 2
+    iIdProyecto: 3
 };
 
